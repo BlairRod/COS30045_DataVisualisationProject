d9c82ff2039e5c9f470bbc669c4a9ea8aa8ad07d
Blair Rod <103892145@student.swin.edu.au>
2024-05-12 04:58:16 +1000

    Chart changing
    Mortality and vaccination chart changes with dropdown.
    added aus-2021 dataset.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var country = "australia";
 // Year the chart will show
 var year = "2020";
 
-var filename = "datasets/australia_2020.csv"
+var filename = "datasets/" + country + "_" + year+ ".csv"
 
 var dataset, width, height, padding, svg, xScale, yScale, xAxis, yAxis, line, area;
 
@@ -13,7 +13,9 @@ function SetCountry() {
 
     // TODO: call function showChasrt(country)
     console.log("Country: " + country + ", Year: " + year);
-    // filename = country + "_" + year+ ".csv"
+    filename = "datasets/" + country + "_" + year+ ".csv"
+    console.log(filename);
+    updateChart()
 }
 
 // Called when year dropdown is selected, changes charts year.
@@ -22,7 +24,57 @@ function SetYear() {
 
     // TODO: call function showChart(country)
     console.log("Country: " + country + ", Year: " + year);
-    // filename = country + "_" + year + ".csv"
+    filename = "datasets/" + country + "_" + year+ ".csv"
+    console.log(filename);
+    updateChart()
+}
+
+function updateChart() {
+    dataset = []
+
+    svg.selectAll("path")
+        .data(dataset)
+        .exit()
+        .remove();
+    
+    drawChart();
+}
+
+function drawChart(){
+    // Get data from file
+    d3.csv(filename, function (d) {
+        return {
+            week: +d.Week,
+            deaths: +d.Deaths,
+            vaccinations: +d.Vaccinations
+        };
+        }).then(function (data) {
+            dataset = data;
+
+            // Set up scale for x
+            xScale = d3.scaleTime()
+                .domain([
+                    d3.min(dataset, function (d) { return d.week; }),
+                    d3.max(dataset, function (d) { return d.week; })
+                ])
+                .range([padding, width]);
+    
+            // Set up scale for y
+            yScale = d3.scaleLinear()
+                .domain([0, d3.max(dataset, function (d) { return d.deaths; })])
+                .range([height - padding, 0]);
+            
+            // Deaths area chart
+            DeathChart(dataset)
+
+            // Vaccinations line chart
+            VaccinationChart(dataset)
+
+            // test console call of table for selected country and year of week and death
+            console.log("Country: " + country + ", Year: " + year);
+            // console.table(dataset, ["week", "deaths", "vaccinations"]);
+        });
+
 }
 
 // Area chart for mortalities
@@ -62,6 +114,7 @@ function DeathChart(dataset) {
 
 }
 
+
 // Line chart for vaccinations
 function VaccinationChart(dataset) {
     
@@ -73,7 +126,7 @@ function VaccinationChart(dataset) {
     // Set up the line
     line = d3.line()
         .x(function (d) { return xScale(d.week); })
-        .y(function (d) { return yScale(d.deaths); });
+        .y(function (d) { return yScale(d.vaccinations); });
     
     // Set up the path
     svg.append("path")
@@ -100,41 +153,9 @@ function init() {
         .attr("height", height);
 
     // TODO: move to function showChart then call
+    drawChart();
 
-    // Get data from 2020 file
-    d3.csv(filename, function (d) {
-        return {
-            week: +d.Week,
-            deaths: +d.Deaths,
-            vaccinations: +d.Vaccinations
-        };
-        }).then(function (data) {
-            dataset = data;
-
-            // Set up scale for x
-            xScale = d3.scaleTime()
-                .domain([
-                    d3.min(dataset, function (d) { return d.week; }),
-                    d3.max(dataset, function (d) { return d.week; })
-                ])
-                .range([padding, width]);
-    
-            // Set up scale for y
-            yScale = d3.scaleLinear()
-                .domain([0, d3.max(dataset, function (d) { return d.deaths; })])
-                .range([height - padding, 0]);
-            
-            // Deaths area chart
-            DeathChart(dataset)
-
-            // Vaccinations line chart
-            VaccinationChart(dataset)
-
-            // test console call of table for selected country and year of week and death
-            console.log("Country: " + country + ", Year: " + year);
-            console.table(dataset, ["week", "deaths", "vaccinations"]);
-        });
-
+   
         // Map
 
         // Bar
