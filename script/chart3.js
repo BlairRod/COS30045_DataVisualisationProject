2ab53e1f5662e58be99425a9516e13edce422d0e
NilangaJude <132471339+NilangaJude@users.noreply.github.com>
2024-05-16 03:49:10 +1000

    x axis broken

---
+++ script/chart3.js
@@ -0,0 +1,86 @@
+function init() {
+    var w = 700;
+    var h = 350; 
+    var padding = 60; 
+
+    // Load data from CSV file using D3
+    d3.csv("./datasets/morality_by_age.csv").then(function(data) {
+        // Log the loaded data to the console
+        console.log(data);
+        var deathData = data;
+
+        // Call the barChart function with loaded data
+        barChart(deathData);
+    });
+
+    // Function to create a bar chart
+    function barChart(data) {
+        // Create scales for x and y axes
+        var xScale = d3.scaleBand()
+            .domain(data.map(function(d) { return d.ages; }))
+            .range([padding, w - padding]) // Adjusted range to accommodate padding
+            .paddingInner(0.1);
+
+        var yScale = d3.scaleLinear()
+            .domain([0, d3.max(data, function(d) { return +d.Deaths; }) * 1.1]) // Adjusted domain to set a maximum value
+            .range([h, 0]);
+
+        // Select the chart container and append an SVG element
+        var svg = d3.select("#deaths_by_age")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h + padding); // Increased height to accommodate padding
+
+        // Create rectangles for each data point
+        svg.selectAll("rect")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("x", function(d) { return xScale(d.ages); })
+            .attr("y", function(d) { return yScale(d.Deaths); })
+            .attr("width", xScale.bandwidth())
+            .attr("height", function(d) { return h - yScale(d.Deaths); })
+            .attr("fill", "steelblue");
+
+        // Add text labels on top of each bar
+        svg.selectAll("text")
+            .data(data)
+            .enter()
+            .append("text")
+            .text(function(d) { return d.Deaths; })
+            .attr("x", function(d) { return xScale(d.ages) + xScale.bandwidth() / 2; })
+            .attr("y", function(d) { return yScale(d.Deaths) - 5; })
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "11px")
+            .attr("fill", "black")
+            .attr("text-anchor", "middle");
+
+        // Create y-axis label
+        svg.append("text")
+            .attr("x", w / 2)
+            .attr("y", h + padding / 1.5) // Adjusted position
+            .attr("text-anchor", "middle")
+            .text("Age Groups");
+
+        // Create x-axis label
+        svg.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("x", -h / 2)
+            .attr("y", padding / 2 ) // Adjusted position
+            .attr("text-anchor", "middle")
+            .text("Number of Deaths")
+            .attr("fill", "black");
+
+        // Create y-axis
+        svg.append("g")
+            .attr("transform", "translate(" + padding + ",0)") // Adjusted position
+            .call(d3.axisLeft(yScale));
+
+        // Create x-axis
+        svg.append("g")
+            .attr("transform", "translate(0," + h + ")")
+            .call(d3.axisBottom(xScale));
+    }
+}
+
+window.onload = init;
