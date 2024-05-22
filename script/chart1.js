ec5f3b66e2a00dfb122fb8e46b87ada7c3746150
Blair Rod <103892145@student.swin.edu.au>
2024-05-22 16:02:02 +1000

    Added axis, labels. Reoved vaccinations.
    Added x and y axis to mortalities/vaccine chart.
    Added labes to  mortalities/vaccine chart.
    Removed vaccinations from mortalities/vaccine chart turning it into mortalitias area chart..

--- script/chart1.js
+++ script/chart1.js
@@ -3,7 +3,7 @@ var country = "australia";
 // Year the chart will show
 var year = "2020";
 
-var filename = "./datasets/" + country + "_" + year+ ".csv"
+var filename = "./datasets/" + country + "_" + year + ".csv"
 
 var dataset, width, height, padding, svg, xScale, yScale, xAxis, yAxis, line, area;
 
@@ -13,8 +13,8 @@ function SetCountry() {
 
     // TODO: call function showChasrt(country)
     console.log("Country: " + country + ", Year: " + year);
-    filename = "datasets/" + country + "_" + year+ ".csv"
-    console.log(filename);
+    filename = "datasets/" + country + "_" + year + ".csv"
+    // console.log(filename);
     updateChart()
 }
 
@@ -24,12 +24,13 @@ function SetYear() {
 
     // TODO: call function showChart(country)
     console.log("Country: " + country + ", Year: " + year);
-    filename = "datasets/" + country + "_" + year+ ".csv"
-    console.log(filename);
+    filename = "datasets/" + country + "_" + year + ".csv"
+    // console.log(filename);
     updateChart()
 }
 
 function updateChart() {
+    // Clear the data
     dataset = []
 
     svg.selectAll("path")
@@ -37,131 +38,118 @@ function updateChart() {
         .exit()
         .remove();
     
-    drawChart();
+    // Clear the axis
+    svg.selectAll("g.x.axis").remove();
+    svg.selectAll("g.y.axis").remove();
+
+    // Clear the labels
+    svg.selectAll("text.x.axis.label").remove();
+    svg.selectAll("text.y.axis.label").remove();
+    
+    DrawChart();
 }
 
-function drawChart(){
+function DrawChart(){
     // Get data from file
     d3.csv(filename, function (d) {
         return {
             week: +d.Week,
-            deaths: +d.Deaths,
-            vaccinations: +d.Vaccinations
+            deaths: +d.Deaths
         };
         }).then(function (data) {
             dataset = data;
 
             // Set up scale for x
-            xScale = d3.scaleTime()
-                .domain([
-                    d3.min(dataset, function (d) { return d.week; }),
-                    d3.max(dataset, function (d) { return d.week; })
-                ])
-                .range([padding, width]);
-    
+            xScale = d3.scaleLinear()
+                .domain([0, d3.max(dataset, function (d) { return d.week; })])
+                .range([padding, width - 5]);
+                    
             // Set up scale for y
             yScale = d3.scaleLinear()
                 .domain([0, d3.max(dataset, function (d) { return d.deaths; })])
                 .range([height - padding, 0]);
-            
-            // Deaths area chart
-            DeathChart(dataset)
 
-            // Vaccinations line chart
-            VaccinationChart(dataset)
+            // Set up the area
+            area = d3.area()
+                .x(function (d) { return xScale(d.week); })
+                .y0(function () { return yScale.range()[0]; })
+                .y1(function (d) { return yScale(d.deaths); });
+            
+            // Create area
+            svg.append("path")
+                .datum(dataset)
+                .attr("class", "area")
+                .attr("d", area);
+
+            // Axis
+            DrawAxis();
 
             // test console call of table for selected country and year of week and death
             console.log("Country: " + country + ", Year: " + year);
-            // console.table(dataset, ["week", "deaths", "vaccinations"]);
         });
 
 }
 
-// Area chart for mortalities
-function DeathChart(dataset) {
 
-    // Set up scale for y
-    yScale = d3.scaleLinear()
-        .domain([0, d3.max(dataset, function (d) { return d.deaths; })])
-        .range([height - padding, 0]);
-
-    // Set up the area
-    area = d3.area()
-        .x(function (d) { return xScale(d.week); })
-        .y0(function () { return yScale.range()[0]; })
-        .y1(function (d) { return yScale(d.deaths); });
-    
-    // Create area
-    svg.append("path")
-        .datum(dataset)
-        .attr("class", "area")
-        .attr("d", area);
-    
+function DrawAxis() {
     // Set up x axis
-    // TODO
-
+    xAxis = d3.axisBottom()
+        .ticks(52)
+        .scale(xScale);
+    
     // Set up y axis
-    // TODO
-
+    yAxis = d3.axisLeft()
+        .scale(yScale);
+    
     // Create x axis
-    // TODO
-
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(" + 0 + ", " + (height - padding) + ")")
+        .call(xAxis);
+    
     // Create y axis
-    // TODO
-
-
-    // TODO: finish area chart
-
-}
-
-
-// Line chart for vaccinations
-function VaccinationChart(dataset) {
+    svg.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + padding + ", " + 0 + ")")
+        .call(yAxis);
     
-    // Set up scale for y
-    yScale = d3.scaleLinear()
-        .domain([0, d3.max(dataset, function (d) { return d.vaccinations; })])
-        .range([height - padding, 0]);
+    // x label
+    svg.append("text")
+        .attr("class", "x axis label")
+        .attr("text-anchor", "middle")
+        .attr("x", width / 2)
+        .attr("y", height)
+        .text("Week Number");
     
-    // Set up the line
-    line = d3.line()
-        .x(function (d) { return xScale(d.week); })
-        .y(function (d) { return yScale(d.vaccinations); });
-    
-    // Set up the path
-    svg.append("path")
-        .datum(dataset)
-        .attr("class", "line")
-        .attr("d", line);
-    
-    // TODO: finish line chart
-
+    // y mortalities label
+    svg.append("text")
+        .attr("class", "y axis label")
+        .attr("text-anchor", "middle")
+        .attr("x", -height / 2)
+        .attr("y", 5)
+        .attr("dy", ".5em")
+        .text("Moratlities")
+        .attr("transform", "rotate(-90)");
 }
 
 
 function init() {
 
     // Height and width of svg
-    width = 600;
-    height = 300;
+    width = 1100;
+    height = 400;
     padding = 55;
 
     // Set up the svg
-    svg = d3.select("#mortality_vs_vaccination_chart")
+    svg = d3.select("#mortality_chart")
         .append("svg")
         .attr("width", width)
         .attr("height", height)
         .attr("class", "svg");
     
-
     // TODO: move to function showChart then call
-    drawChart();
+    DrawChart();
 
-   
-        // Map
-
-        // Bar
-        
 }
 
 window.onload = init;
