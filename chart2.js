0e7ff8b48e6b3274f3aa4c2a3be4edfeb69fc108
Blair Rod <103892145@student.swin.edu.au>
2024-06-05 02:40:51 +1000

    chart 2 zoom and pan
    Added zoom buttons.
    mouse whell zooms.
    double click zooms in.
    updated/shrunk pan buttons..

--- chart2.js
+++ chart2.js
@@ -10,14 +10,16 @@ function init() {
     var color = d3.scaleOrdinal(d3.schemeSet2);
     // var color = d3.scaleOrdinal(d3.schemeSet1);
 
+    // Format for thousan value (1000 => 1,000)
+	var thousandFormat = d3.format(",");
+
     // Projection
     var projection = d3.geoKavrayskiy7()
-        .scale(170)
-        .translate([width / 2, height / 2])
-        .precision(0.1);
+        // .scale(170)
+        // .translate([width / 2, height / 2])
+        // .precision(0.1);
+        .translate([0, 0]);
 
-
-    
     // Path
     var path = d3.geoPath().projection(projection);
 
@@ -27,31 +29,71 @@ function init() {
         .attr("width", width)
         .attr("height", height);
     
-    // Function to change translate of chart during mouse dragging
-    var dragging = function (d) {
-        // Current translate offset
-        var offset = projection.translate();
+    // // Function to change translate of chart during mouse dragging
+    // var dragging = function (d) {
+    //     // Current translate offset
+    //     var offset = projection.translate();
 
-        //  Offset following mouse
-        offset[0] += d3.event.dx;
-        offset[1] += d3.event.dy;
+    //     //  Offset following mouse
+    //     offset[0] += d3.event.dx;
+    //     offset[1] += d3.event.dy;
+
+    //     // Update projection
+    //     projection.translate(offset);
+
+    //     // Update
+    //     svg.selectAll("path")
+    //         .attr("d", path);
+    // }
+
+    // // Definition of drag behavior
+    // var drag = d3.drag()
+    //     .on("drag", dragging);
+
+    // // SVG group container for pannable elemens
+    // var map = svg.append("g")
+    //     .attr("id", "map")
+    //     .call(drag);
+
+    // Function for panning and zooming
+    var zooming = function (d) {
+        // Console log for event
+        // console.log(d3.event.transform);
+
+        // Offset array
+        var offset = [
+            d3.event.transform.x,
+            d3.event.transform.y
+        ];
+
+        // Calculates scale
+        var newScale = d3.event.transform.k * 2000;
 
         // Update projection
-        projection.translate(offset);
+        projection.translate(offset)
+            .scale(newScale);
 
-        // Update
+        // Update path
         svg.selectAll("path")
             .attr("d", path);
     }
 
-    // Definition of drag behavior
-    var drag = d3.drag()
-        .on("drag", dragging);
+    // Definition of zoom behaviour
+    var zoom = d3.zoom()
+        .on("zoom", zooming);
+    
+    // Map center
+    var center = projection([485.0, 290.0]);
 
-    // SVG group container for pannable elemens
+    // SVG group container for pannable and zoomable elemens
     var map = svg.append("g")
         .attr("id", "map")
-        .call(drag);
+        .call(zoom)
+        .call(zoom.transform, d3.zoomIdentity
+            .translate(width - 1400, height - 700)
+            .scale(0.25)
+            .translate(-center[0], -center[1])
+        );
     
     // Invisible rect to catch mouse events
     map.append("rect")
@@ -91,8 +133,6 @@ function init() {
         .defer(d3.csv, "world_population.csv")
         .await(ready);
         
-
-
     function ready(error, world, names, pops) {
         if (error) throw error;
 
@@ -141,7 +181,7 @@ function init() {
             })
             .call(d3.helper.tooltip(function (d, i) {
                 // Info shown in tooltip, currently "country name: country population"
-                return ("<b>" + countryArr[d.id].name +  "</br>" + "Population: " + countryArr[d.id].pop + "</b>");
+                return ("<b>" + countryArr[d.id].name +  "</br>" + "Population: " + thousandFormat(countryArr[d.id].pop) + "</b>");
             }));
         
         // svg.insert("path", ".graticule")
@@ -152,107 +192,185 @@ function init() {
         //     .attr("d", path);
 
         panButtons()
+        zoomButtons();
     };
     
     // d3.select(self.frameElement).style("height", height + "px");
 
+    // Creates buttons for panning
     var panButtons = function () {
 
         // Top button for panning north
         var top_button = svg.append("g")
             .attr("class", "pan")
-            .attr("id", "top_button");
+            .attr("id", "top_button")
+            .attr("transform", "translate(" + (75) + "," + (height - 85) + ")");
         
         top_button.append("rect")
             .attr("x", 0)
             .attr("y", 0)
-            .attr("width", width)
+            .attr("width", 20)
             .attr("height", 25);
         
         top_button.append("text")
-            .attr("x", width / 2)
-            .attr("y", 17)
+            .attr("x", 10)
+            .attr("y", 18)
             .html("&#8679;");
         
         // Bottom button for panning south
         var bottom_button = svg.append("g")
             .attr("class", "pan")
-            .attr("id", "bottom_button");
+            .attr("id", "bottom_button")
+            .attr("transform", "translate(" + (75) + "," + (height - 40) + ")");
         
         bottom_button.append("rect")
             .attr("x", 0)
-            .attr("y", height - 25)
-            .attr("width", width)
+            .attr("y", 0)
+            .attr("width", 20)
             .attr("height", 25);
         
         bottom_button.append("text")
-            .attr("x", width / 2)
-            .attr("y", height - 6)
+            .attr("x", 10)
+            .attr("y", 18)
             .html("&#8681;");
         
         // Left button for panning west
         var left_button = svg.append("g")
             .attr("class", "pan")
-            .attr("id", "left_button");
+            .attr("id", "left_button")
+            .attr("transform", "translate(" + (50) + "," + (height - 60) + ")");
         
         left_button.append("rect")
             .attr("x", 0)
-            .attr("y", 25)
+            .attr("y", 0)
             .attr("width", 25)
-            .attr("height", height - 50);
+            .attr("height", 20);
         
         left_button.append("text")
-            .attr("x", 10)
-            .attr("y", height / 2)
+            .attr("x", 11)
+            .attr("y", 15)
             .html("&#8678;");
         
         // Right button for panning east
         var right_button = svg.append("g")
             .attr("class", "pan")
-            .attr("id", "right_button");
+            .attr("id", "right_button")
+            .attr("transform", "translate(" + (95) + "," + (height - 60) + ")");
         
         right_button.append("rect")
-            .attr("x", width - 25)
-            .attr("y", 25)
+            .attr("x", 0)
+            .attr("y", 0)
             .attr("width", 25)
-            .attr("height", height - 50);
+            .attr("height", 20);
         
         right_button.append("text")
-            .attr("x", width - 11)
-            .attr("y", height / 2)
+            .attr("x", 12)
+            .attr("y", 15)
             .html("&#8680;");
         
         // Movement on click function
         d3.selectAll(".pan")
             .on("click", function () {
                 // Current position
-                var current = projection.translate();
+                // var current = projection.translate();
                 // Distance to move on click
-                var moveDist = 30;
+                var moveDist = 100;
+                var x = 0;
+                var y = 0;
                 // Direction to move on click
                 var direction = d3.select(this).attr("id")
                 switch (direction) {
                     case "top_button":
-                        current[1] += moveDist;
+                        y += moveDist;
                         break;
                     case "bottom_button":
-                        current[1] -= moveDist;
+                        y -= moveDist;
                         break;
                     case "left_button":
-                        current[0] += moveDist;
+                        x += moveDist;
                         break;
                     case "right_button":
-                        current[0] -= moveDist;
+                        x -= moveDist;
                         break;
                 }
-                projection.translate(current);
-                svg.selectAll("path")
-                    .transition()
-                    .attr("d", path);
+
+                // projection.translate(current);
+                // svg.selectAll("path")
+                //     .transition()
+                //     .attr("d", path);
+
+                map.transition()
+                    .call(zoom.translateBy, x, y);
                 
             });
 
     }
+
+    // Creates buttons for zooming
+    var zoomButtons = function () {
+
+        // Top button for zooming in
+        var zoom_in = svg.append("g")
+            .attr("class", "zoom")
+            .attr("id", "zoom_in")
+            .attr("transform", "translate(" + (width - 110) + "," + (height - 70) + ")");
+        
+        zoom_in.append("rect")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("rx", 15)
+            .attr("ry", 15)
+            .attr("width", 30)
+            .attr("height", 30);
+        
+        zoom_in.append("text")
+            .attr("x", 15)
+            .attr("y", 20)
+            .html("&#43;");
+        
+        // Bottom button for zooming out
+        var zoom_out = svg.append("g")
+            .attr("class", "zoom")
+            .attr("id", "zoom_out")
+			.attr("transform", "translate(" + (width - 70) + "," + (height - 70) + ")");
+        
+        zoom_out.append("rect")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("rx", 15)
+            .attr("ry", 15)
+            .attr("width", 30)
+            .attr("height", 30);
+        
+        zoom_out.append("text")
+            .attr("x", 15)
+            .attr("y", 20)
+            .html("&#8722;");
+        
+
+        // Zoom button function
+        d3.selectAll(".zoom")
+            .on("click", function () {
+                var scaleFactor;
+
+                var direction = d3.select(this).attr("id");
+
+                switch (direction) {
+                    case "zoom_in":
+                        scaleFactor = 1.5;
+                        break;
+                    case "zoom_out":
+                        scaleFactor = 0.75;
+                        break;
+                    default:
+                        break;
+                }
+                map.transition()
+                    .call(zoom.scaleBy, scaleFactor);
+
+            });
+    }
+
 
 }
 
